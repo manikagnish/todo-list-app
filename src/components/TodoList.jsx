17172abb985807765da92dafa728b523170ceb4b
manikagnish <72827130+manikagnish@users.noreply.github.com>
2021-12-14 14:10:56 +0530

    added active and completed list in localStorage

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,4 +1,4 @@
-import { useReducer, useContext, useEffect } from 'react';
+import { useReducer, useContext, useEffect, useState } from 'react';
 import { GlobalContext } from '../context/GlobalContext';
 import { reducer } from '../context/AppReducer';
 import Todo from './Todo';
@@ -11,6 +11,7 @@ export default function TodoList() {
   );
   const { getName } = useContext(GlobalContext);
   const [name, setName] = getName;
+  const [listName, setListName] = useState('all');
 
   const handleSubmit = e => {
     e.preventDefault();
@@ -18,9 +19,43 @@ export default function TodoList() {
     setName('');
   };
 
+  const completed = [];
+  const active = [];
+
   useEffect(() => {
     localStorage.setItem('todos', JSON.stringify(todos));
-  }, [todos]);
+
+    for (let i = 0; i < todos.length; i++) {
+      if (todos[i].complete) {
+        completed.push(todos[i]);
+        localStorage.setItem('completedTodos', JSON.stringify(completed));
+      } else if (!todos[i].complete) {
+        active.push(todos[i]);
+        localStorage.setItem('activeTodos', JSON.stringify(active));
+      }
+    }
+  }, [todos, active, completed]);
+
+  function displayList() {
+    switch (listName) {
+      case 'all':
+        return todos.map(todo => (
+          <Todo key={todo.id} todo={todo} dispatch={dispatch} />
+        ));
+      case 'completed':
+        const arr = JSON.parse(localStorage.getItem('completedTodos')) || [];
+        return arr.map(todo => (
+          <Todo key={todo.id} todo={todo} dispatch={dispatch} />
+        ));
+      case 'active':
+        return JSON.parse(localStorage.getItem('activeTodos')).map(todo => (
+          <Todo key={todo.id} todo={todo} dispatch={dispatch} />
+        ));
+
+      default:
+        return [];
+    }
+  }
 
   return (
     <div>
@@ -33,9 +68,15 @@ export default function TodoList() {
         />
       </form>
       <ul>
-        {todos.map(todo => (
-          <Todo key={todo.id} todo={todo} dispatch={dispatch} />
-        ))}
+        {displayList()}
+
+        <li className="p-4 bg-green-400 list-none">
+          <nav className="flex justify-between">
+            <button onClick={() => setListName('all')}>all</button>
+            <button onClick={() => setListName('active')}>active</button>
+            <button onClick={() => setListName('completed')}>completed</button>
+          </nav>
+        </li>
       </ul>
     </div>
   );
